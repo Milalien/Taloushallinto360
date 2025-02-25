35574f8d33e3103f46911b223903620f71248c93
Milla Kärkkäinen <karkkimilla@gmail.com>
2025-02-25 08:36:34 +0200

    edits

--- js/animation.js
+++ js/animation.js
@@ -1,45 +1,48 @@
-$(document).ready(function(){
-    $(".numbers1").mouseenter(function(event){
-     $(event.currentTarget).animate({left:'20px'});
-   });
-   $(".numbers1").mouseenter(function(event){
-     $(event.currentTarget).animate({top:'20px'});
-   });
-   $('.numbers1').mouseleave(function(event){
- $(event.currentTarget).animate({top : 0});
-     });
-     $('.numbers1').mouseleave(function(event){
- $(event.currentTarget).animate({left : 0});
-     });
- });
 
- $(document).ready(function(){
-    $(".numbers2").mouseenter(function(event){
-     $(event.currentTarget).animate({right:'20px'});
-   });
-   $(".numbers2").mouseenter(function(event){
-     $(event.currentTarget).animate({top:'20px'});
-   });
-   $('.numbers2').mouseleave(function(event){
- $(event.currentTarget).animate({top : 0});
-     });
 
-     $('.numbers2').mouseleave(function(event){
- $(event.currentTarget).animate({right : 0});
-     });
- });
 
- $(document).ready(function(){
-   $(".numbers3").mouseenter(function(event){
-     $(event.currentTarget).animate({bottom:'20px'});
-   });
-   $('.numbers3').mouseleave(function(event){
- $(event.currentTarget).animate({top :'20px'});
-     });
-     $(".numbers3").mouseenter(function(event){
-      $(event.currentTarget).animate({bottom: 0});
-    });
-    $('.numbers3').mouseleave(function(event){
-  $(event.currentTarget).animate({top : 0});
-      });
- });
+$(document).ready(function () {
+  $(".numbers1").mouseenter(function (event) {
+    $(event.currentTarget).animate({ left: '20px' });
+  });
+  $(".numbers1").mouseenter(function (event) {
+    $(event.currentTarget).animate({ bottom: '50px' });
+  });
+  $('.numbers1').mouseleave(function (event) {
+    $(event.currentTarget).animate({ bottom: 0 });
+  });
+  $('.numbers1').mouseleave(function (event) {
+    $(event.currentTarget).animate({ left: 0 });
+  });
+});
+
+$(document).ready(function () {
+  $(".numbers2").mouseenter(function (event) {
+    $(event.currentTarget).animate({ right: '20px' });
+  });
+  $(".numbers2").mouseenter(function (event) {
+    $(event.currentTarget).animate({ top: '20px' });
+  });
+  $('.numbers2').mouseleave(function (event) {
+    $(event.currentTarget).animate({ top: 0 });
+  });
+
+  $('.numbers2').mouseleave(function (event) {
+    $(event.currentTarget).animate({ right: 0 });
+  });
+});
+
+$(document).ready(function () {
+  $(".numbers3").mouseenter(function (event) {
+    $(event.currentTarget).animate({ bottom: '60px' });
+  });
+  $('.numbers3').mouseleave(function (event) {
+    $(event.currentTarget).animate({ top: '20px' });
+  });
+  $(".numbers3").mouseenter(function (event) {
+    $(event.currentTarget).animate({ bottom: 0 });
+  });
+  $('.numbers3').mouseleave(function (event) {
+    $(event.currentTarget).animate({ top: 0 });
+  });
+});
